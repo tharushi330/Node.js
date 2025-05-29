98e2e947076a3e1e7d76efc8da025167f51296c7
tharushi330 <tharushiwarangana78@gmail.com>
2025-05-29 11:42:28 +0530

    import ,export

---
+++ myFunctions.js
@@ -0,0 +1,15 @@
+const math = (number1, number2, number3, number4) => {   // arrow function
+    let a = number1 + number2 * 2;
+    if (number3) {
+        a += number3 - 5;
+    }
+    
+    if (number4) {
+        a *= number4;
+    }
+    return a;
+};
+
+export { math }; //me hadala tyena function eka wena file ekakata denna onenam export krnawa me file eke(object destructuer)
+// export default math; 
+
---
+++ test.js
@@ -0,0 +1,4 @@
+import { math }  from "./myFunctions.js";   //wena file ekaka thiyena function ekak me file ekata gaddi import karanwa me file eke(object destructuer)
+// import math from "./myFunctions.js";
+
+console.log(math(2, 3));

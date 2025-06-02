4622583023606883cfff63448b2fc1fff9a5b566
tharushi330 <tharushiwarangana78@gmail.com>
2025-06-02 08:13:54 +0530

    file read

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
-const os = require('os'); //os package eka aragaththa(import) vidiya
-const path = require('path');
+// const os = require('os'); //os package eka aragaththa(import) vidiya
+// const path = require('path');
 
 // import os  from "os"; //import karana aluth vidiya
 // import path from "path";
-// import { fileURLToPath } from "url";
-// const __filename = fileURLToPath(import.meta.url);
+// import os  from "node:os"; //mehema dammama kiyanne node ekenma os eka ganna kiyala(node wala thiyena package ekak aragannawanm)
+import { dirname, join } from "node:path"; //file read karaddi path ekata join eka gnnath oni (join eken path module eka create karanawa)
+import { readFile } from "fs"; //file ekak read karaganna meka import karagnna one
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename); // file read kraddi dana vidiya
 
 
 
-//------ os eken balagnna puluwan dewal------
-console.log(os.platform());
-console.log(os.version());
-console.log(__dirname); //directory eke full path eka ganna
-console.log(__filename); //filename ekath ekka path eka araganna
+// //------ os eken balagnna puluwan dewal------
+// console.log(os.platform());
+// console.log(os.version());
+// console.log(__dirname); //directory eke full path eka ganna
+// console.log(__filename); //filename ekath ekka path eka araganna
 
 
-//------ path eken balagnna puluwan dewal-------
-console.log(path.basename(__filename)); //filename eka vitharak ganna ona unoth
-console.log(path.extname(__filename)); //file extention eka ganna(html or css or js)
+// //------ path eken balagnna puluwan dewal-------
+// console.log(path.basename(__filename)); //filename eka vitharak ganna ona unoth
+// console.log(path.extname(__filename)); //file extention eka ganna(html or css or js)
+
+    
+        // file read function
+ readFile(join(__dirname, "myname.txt"), (err, data) => {
+    if(err){
+        console.log(err);
+    }
+    console.log(data.toString());  //txt file eke liyapuwa print wenwa console eke
+    
+});
+
+
 
 
 
--- myFunctions.js
+++ myFunctions.js
@@ -12,4 +12,5 @@ const math = (number1, number2, number3, number4) => {   // arrow function
 
 export { math }; //me hadala tyena function eka wena file ekakata denna onenam export krnawa me file eke(object destructuer)
 // export default math; 
+// export { math ,abc , aaa}; // function godak hadala e hama function ekakma "," dala export karanna puluwn.anith file eken import krnna oni
 
